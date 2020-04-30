d1548d64f55632abca60a43cc379be504709e79a
Marie-Laure <marie-laure.riedinger@le-campus-numerique.fr>
2020-04-30 15:18:36 +0200

    Mise en place serveur + ajout de la zone de dessin

---
+++ server/app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var http = require("http");
+var app = express(); // On appelle une instance du module express
+
+// On récupère le serveur du module http de nodejs, on y ajoute express et on écoute le port 80
+// On écoute le port 80, aucune route n'est définie pour le moment
+http.Server(app).listen(80);
+() => {
+  console.log("listen on 80");
+};
+
+// On utilise express pour qu'au chargement de la page, quand le navigateur renvoie "/", on appelle le dossier "client" qui contient l'index.html
+app.use("/", express.static(__dirname + "/../client")); // static rend le dossier public tel quel, il est consultable dans la console > Sources
